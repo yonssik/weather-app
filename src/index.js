29f526ff1ee34f0e6d0adcc961678f4e88a3e6f3
yonssik <44497456+yonssik@users.noreply.github.com>
2020-06-22 02:23:13 +0300

    deploy

--- src/index.js
+++ src/index.js
@@ -14,16 +14,15 @@ import reducer from './store/reducer/reducer';
 const logger = store => {
   return next => {
     return action => {
-      //console.log('[Middleware] Dispatching ', action);
+      console.log('[Middleware] Dispatching ', action);
       const result = next(action);
-      //console.log('[Middleware] next state ', store.getState());
+      console.log('[Middleware] next state ', store.getState());
       return result;
     };
   };
 };
 
 const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
-//const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(reducer, composeEnhancers(applyMiddleware(logger, thunk)));
 
